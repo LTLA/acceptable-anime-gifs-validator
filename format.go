2fda372520311a5b08d10b84493153fa1723ab22
LTLA <infinite.monkeys.with.keyboards@gmail.com>
2021-11-24 13:17:38 -0800

    Streamlined directory traversal, clarified function interface.

--- format.go
+++ format.go
@@ -8,7 +8,7 @@ import (
     "errors"
     "fmt"
     "flag"
-    "io/fs"
+    "io/ioutil"
 )
 
 // Definitions of structs and constants. 
@@ -31,8 +31,8 @@ const suffix = ".json"
 
 // Loading functions.
 
-func LoadGifMetadata(path string) (*GifInfo, error) {
-    handle, err := os.Open(path)
+func LoadGifMetadata(dir, show, base string) (*GifInfo, error) {
+    handle, err := os.Open(filepath.Join(dir, show, base))
     if (err != nil) {
         return nil, err
     }
@@ -46,32 +46,23 @@ func LoadGifMetadata(path string) (*GifInfo, error) {
         return nil, err
     }
 
-    parts := strings.Split(path, string(os.PathSeparator))
-    if (len(parts) < 2) {
-        return nil, errors.New("'path' should contain a subdirectory with the show ID")
-    }
-    base := parts[len(parts) - 1]
-    show := parts[len(parts) - 2]
-    output.ShowId = show
-
     if (len(base) == len(suffix)) {
-        return nil, errors.New("'path' should contain a non-empty name before '" + suffix + "'")
+        return nil, errors.New("'base' should contain a non-empty name before '" + suffix + "'")
     }
     prefix := base[:len(base) - len(suffix)]
     gif_name := prefix + ".gif"
     output.Path = show + "/" + gif_name // don't use filepath.Join, we're making URL components here.
 
-    parts[len(parts) - 1] = gif_name
-    checkpath := filepath.Join(parts...)
+    checkpath := filepath.Join(dir, show, gif_name)
     if _, err := os.Stat(checkpath); os.IsNotExist(err) {
-        return nil, errors.New("GIF should exist at '" + path + "'")
+        return nil, errors.New("GIF should exist at '" + checkpath + "'")
     }
 
     return &output, nil
 }
 
-func LoadShowMetadata(dir string) (*ShowInfo, error) {
-    path := filepath.Clean(dir) + ".json"
+func LoadShowMetadata(dir, show string) (*ShowInfo, error) {
+    path := filepath.Join(dir, filepath.Clean(show) + ".json")
 
     handle, err := os.Open(path)
     if (err != nil) {
@@ -95,63 +86,50 @@ func LoadShowMetadata(dir string) (*ShowInfo, error) {
 func CollateMetadata(dir string) ([]GifInfo, []ShowInfo, error) {
     var gifs []GifInfo
     var shows []ShowInfo
-    show_ptrs := make(map[string]*ShowInfo)
 
-    err := filepath.Walk(dir, func(path string, info fs.FileInfo, err error) error {
-        if (info.IsDir()) {
-            if (dir != path) {
-                show_info, err := LoadShowMetadata(path)
-                if (err != nil) {
-                    return err
-                }
-                shows = append(shows, *show_info)
-
-                _, base := filepath.Split(path)
-                show_ptrs[base] = show_info
-            }
-            return nil
-        }
-
-        // Avoid loading in show metadata, if this occurs inside 'dir' without further nesting.
-        stub, err := filepath.Rel(dir, path)
-        if (err != nil) {
-            return err
-        }
-
-        subdir, _ := filepath.Split(stub)
-        if (subdir == "" || !strings.HasSuffix(path, suffix)) {
-            return nil
-        }
-
-        gif_info, err := LoadGifMetadata(path)
-        if (err != nil) {
-            return err
-        }
-        gifs = append(gifs, *gif_info)
-
-        return nil
-    })
-
+    entries, err := ioutil.ReadDir(dir)
     if (err != nil) {
         return nil, nil, err
     }
 
-    // Looping through and checking that each GIF has valid characters.
-    // Also updating the show ID to use the actual ID, not our path.
-    for i, x := range gifs {
-        curshow, found := show_ptrs[x.ShowId]
-        if (!found) {
-            return nil, nil, errors.New("did not find show-level metadata for '" + x.Path + "'")
-        }
-
-        for _, y := range x.Characters {
-            _, found := curshow.Characters[y]
-            if (!found) {
-                return nil, nil, errors.New("did not find listing for '" + y + "' in '" + x.Path + "'")
+    for _, info := range entries {
+        if (info.IsDir()) {
+            show_info, err := LoadShowMetadata(dir, info.Name())
+            if (err != nil) {
+                return nil, nil, err
             }
-        }
+            shows = append(shows, *show_info)
 
-        gifs[i].ShowId = curshow.Id
+            subentries, err := ioutil.ReadDir(filepath.Join(dir, info.Name()))
+            if (err != nil) {
+                return nil, nil, err
+            }
+
+            for _, subinfo := range subentries {
+                if (!strings.HasSuffix(subinfo.Name(), ".json")) {
+                    continue
+                }
+
+                gif_info, err := LoadGifMetadata(dir, info.Name(), subinfo.Name())
+                if (err != nil) {
+                    return nil, nil,err
+                }
+
+                // Updating the show ID to use the actual ID, not our path.
+                gif_info.ShowId = show_info.Id
+
+                // Checking that each GIF has valid characters.
+                for _, y := range gif_info.Characters {
+                    _, found := show_info.Characters[y]
+                    if (!found) {
+                        return nil, nil, errors.New("did not find listing for '" + y + "' in '" + gif_info.Path + "'")
+                    }
+                }
+
+                gifs = append(gifs, *gif_info)
+            }
+
+        }
     }
 
     return gifs, shows, nil
